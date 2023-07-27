53dbceb940a582f726ab83f23504e745a34c5fe4
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-26 20:22:55 -0400

    Initial commit

---
+++ app/src/main/java/cl/jpvs/mod6ej01/MainActivity.kt
@@ -0,0 +1,26 @@
+package cl.jpvs.mod6ej01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+ [] Navigation
+ [] Fragmento agregar
+[] Fragmento Listado
+ [] Crear Interfaz
+
+ [] Room
+   [] _Dao
+   [] _Base Datos
+   [] _Entity
+
+ [] Fragmento Listado
+[] RecyclerView
+
+*/
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
