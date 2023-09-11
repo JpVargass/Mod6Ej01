f037ce411aa69e6a2b29031f8d38f90f516d1980
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-09-11 00:07:04 -0300

    Viewmodel listo sin ocupar

--- app/src/main/java/cl/jpvs/mod6ej01/TareaVM.kt
+++ app/src/main/java/cl/jpvs/mod6ej01/TareaVM.kt
@@ -2,7 +2,22 @@ package cl.jpvs.mod6ej01
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class TareaVM (aplicacion: Application) : AndroidViewModel(aplicacion) {
+    private val repositorio : Repositorio
+    init {
+        repositorio = Repositorio(TareaBaseDatos.getDatabase(aplicacion).getTaskDao())
 
+    }
+  fun obtenerTareas(): LiveData<List<Tarea>> {
+      return repositorio.getTareas()
+
+  }
+
+ fun insertarTareas(tarea: Tarea)= viewModelScope.launch {
+     repositorio.insertTask(tarea)
+ }
 }
